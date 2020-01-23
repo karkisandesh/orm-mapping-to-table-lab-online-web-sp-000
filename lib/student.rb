2485eb0b7b8111279b5de0b332d5f67ad460ac03
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-23 05:04:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  attr_reader :
+  attr_reader :id 
+  attr_accessor :name, :grade
+  
   
 end
