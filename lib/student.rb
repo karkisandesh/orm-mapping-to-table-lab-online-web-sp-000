8143e3f0aad2089f07030fc5d2fb3e2fa427d9d9
Sandesh Karki <karkisandesh@hotmail.com>
2020-01-23 05:24:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
     
   def self.drop_table
     sql = <<-SQL
-       CREATE TABLE IF NOT EXISTS students (
+       DROP IF EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
         grade TEXT
